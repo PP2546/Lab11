9c2860a5c277a566162c7c1e62fdf70aee0e06bd
PP <phiphatphong_ka@cmu.ac.th>
2023-01-03 16:05:44 +0700

    Full

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,28 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int number;
+    float sum;
+    string text;
+    float ans;
+    ifstream num;
+    num.open ("score.txt");
+    while(getline(num,text)){
+        sum += stof(text);
+        ans += pow(stof(text),2);
+        number++;
+    }
+ 
+    cout << "Number of data = " << number << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << sum/number << "\n";
+    cout << "Standard deviation = " <<  sqrt((ans/number)-pow((sum/number),2));
+    num.close();
+    
+    return 0;
+}
